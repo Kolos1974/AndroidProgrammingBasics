1b0332e6c803f6cc77c7a958ae809b45b42d5bb7
Kolos1974 <karlovitz.kolos@gmail.com>
2022-05-29 21:58:22 +0200

    calculatordemo fixed

--- CalculatorDemo/app/src/main/java/com/example/calculatordemo/MainActivity.kt
+++ CalculatorDemo/app/src/main/java/com/example/calculatordemo/MainActivity.kt
@@ -6,16 +6,48 @@ import com.example.calculatordemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //val numA = binding.etNumA.text.toString().toInt()
-        //val numB = binding.etNumB.text.toString().toInt()
+        binding.btnSum.setOnClickListener {
 
-        //binding.tvResult.text = "Result: ${numA+numB}"
+            try {
+                // var numA: Long  = 0
+                var numA = 0
+                var numB = 0
+
+                if (binding.etNumA.text.isNotEmpty()) {
+                    numA = binding.etNumA.text.toString().toInt()
+                }
+
+                if (binding.etNumB.text.isNotEmpty()) {
+                    numB = binding.etNumB.text.toString().toInt()
+                }
+
+                // binding.tvResult.text = "Result: ${numA + numB}"
+                binding.tvResult.text = getString(R.string.text_result, numA + numB)
+
+                /*
+                if (binding.etNumA.text.isNotEmpty()) {
+                    if (binding.etNumB.text.isNotEmpty()) {
+                        val numA = binding.etNumA.text.toString().toInt()
+                        val numB = binding.etNumB.text.toString().toInt()
+                        binding.tvResult.text = "Result: ${numA + numB}"
+                    } else {
+                        binding.etNumB.error = "Hiba"
+                    }
+                } else {
+                    binding.etNumA.error = "Hiba"
+                }
+                */
+            } catch (e: Exception) {
+                binding.tvResult.text = "Error, the number is too large: ${e.message}"
+            }
+
+        }
     }
 }
